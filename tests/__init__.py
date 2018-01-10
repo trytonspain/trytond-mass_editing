a728eeb07f7ab62229a5e0ecb8e26ce73408045d
C?dric Krier <ced@b2ck.com>
2018-01-10 11:16:57 +0100

    Remove volumes from drone and fix setup for test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from .test_mass_editing import suite
+try:
+    from trytond.modules.mass_editing.tests.test_mass_editing import suite
+except ImportError:
+    from .test_mass_editing import suite
+
+__all__ = ['suite']
